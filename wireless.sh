c13b9975b507850081367847562f1f3395971715
Brian Buccola <brian.buccola@gmail.com>
2014-09-09 18:47:12 -0400

    added `wireless.sh', which computes and prints wifi essid and quality; for use with xmobar

---
+++ wireless.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo iwconfig wlp1s0 2>&1 | grep -q no\ wireless\ extensions\. && {
+  echo wired
+  exit 0
+}
+
+sudo iwconfig wlp1s0 2>&1 | grep -q ESSID:off && {
+  echo "(no wifi)"
+  exit 0
+}
+
+essid=$(sudo iwconfig wlp1s0 | awk -F '"' '/ESSID/ {print $2}')
+quality=$(sudo iwconfig wlp1s0 | awk -F '=' '/Quality/ {print $2}' | cut -d ' ' -f 1)
+quality_pct=$(echo "$quality * 100" | bc -l | sed 's/\.[0-9]*//')
+
+echo $essid $quality_pct\%
+exit 0
