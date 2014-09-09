6a0b99cdfee40b9a474838876850870d634b520f
Brian Buccola <brian.buccola@gmail.com>
2014-09-09 18:45:02 -0400

    added `brightness-dec.sh' and `brightness-inc.sh' for decreasing and increasing brightness, since Lenovo brightness keys do not work

---
+++ brightness-dec.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+max_brightness=255
+min_brightness=5
+current_brightness=$(cat /sys/class/backlight/radeon_bl0/brightness)
+dec_amt=20
+new_brightness=$(echo $(($current_brightness - $dec_amt)))
+
+if [[ $new_brightness -gt $max_brightness ]]; then
+  echo $max_brightness | sudo tee /sys/class/backlight/radeon_bl0/brightness
+elif [[ $new_brightness -lt $min_brightness ]]; then
+  echo $min_brightness | sudo tee /sys/class/backlight/radeon_bl0/brightness
+else
+  echo "$new_brightness" | sudo tee /sys/class/backlight/radeon_bl0/brightness
+fi
