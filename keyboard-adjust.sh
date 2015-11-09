698f7ddff5543d382f73a7ab757f8ca4224695c0
Brian Buccola <brian.buccola@gmail.com>
2015-11-09 23:58:35 +0200

    swapped US layout with Israeli one

--- keyboard-adjust.sh
+++ keyboard-adjust.sh
@@ -6,7 +6,7 @@
 #
 # description:      Set up kbd layouts, and switch caps lock with ctrl_L
 
-setxkbmap -layout dvp,de,it,us          # programmers-dvorak kbd primary
+setxkbmap -layout dvp,de,it,il          # programmers-dvorak kbd primary
 setxkbmap -option                       # clear options
 setxkbmap -option "grp:shifts_toggle"   # use both shifts to toggle layout
 setxkbmap -option "ctrl:swapcaps"       # swap ctrl_L and caps lock
