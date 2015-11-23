fd45a2d6128d34bd57e7a4576b399ca488067e71
Brian Buccola <brian.buccola@gmail.com>
2015-11-23 22:26:50 +0200

    added `sudo' to make `iwconfig' output quality info

--- wireless.sh
+++ wireless.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-iwconfig wlp1s0 2>&1 | grep -q no\ wireless\ extensions\. && {
+sudo iwconfig wlp1s0 2>&1 | grep -q no\ wireless\ extensions\. && {
   echo wired
   exit 0
 }
 
-iwconfig wlp1s0 2>&1 | grep -q ESSID:off && {
+sudo iwconfig wlp1s0 2>&1 | grep -q ESSID:off && {
   echo "(no wifi)"
   exit 0
 }
 
-essid=$(iwconfig wlp1s0 | awk -F '"' '/ESSID/ {print $2}')
-quality=$(iwconfig wlp1s0 | awk -F '=' '/Quality/ {print $2}' | cut -d ' ' -f 1)
+essid=$(sudo iwconfig wlp1s0 | awk -F '"' '/ESSID/ {print $2}')
+quality=$(sudo iwconfig wlp1s0 | awk -F '=' '/Quality/ {print $2}' | cut -d ' ' -f 1)
 quality_pct=$(echo "$quality * 100" | bc -l | sed 's/\.[0-9]*//')
 
 echo $essid $quality_pct\%
