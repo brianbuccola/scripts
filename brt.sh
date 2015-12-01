9c9fde69204e91b5f2c2ac784f9cb5a0e66347d0
Brian Buccola <brian.buccola@gmail.com>
2015-12-01 14:40:41 +0200

    fix bugs, add exceed max/min checks before setting brightness

--- brt.sh
+++ brt.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#
+# file:           brt.sh
+# author:         Brian Buccola
+#
+# description:    Increase screen brightness by `$inc_amt`. Useful for binding
+#                 a key to, when your laptop's built-in <Fn-BrightnessUp> key
+#                 doesn't work.
 
 max_brightness=255
 min_brightness=5
@@ -7,45 +14,38 @@ up_amt=20
 down_amt=20
 
 brt_change() {
-  local amt=$1
-  echo "$amt" | sudo tee /sys/class/backlight/radeon_bl0/brightness
+  echo "$1" | sudo tee /sys/class/backlight/radeon_bl0/brightness
 }
 
 brt_up() {
   local new_brightness=$(($current_brightness + $up_amt))
-  brt $new_brightness
+  if [[ $new_brightness -le $max_brightness ]]; then
+    brt_change "$new_brightness"
+  else
+    brt_change "$max_brightness"
+  fi
 }
 
 brt_down() {
   local new_brightness=$(($current_brightness - $up_amt))
-  brt $new_brightness
-}
-
-exceeds_max() {
-  local new_brightness=$1
-  if [[ $new_brightness -gt $max_brightness ]]; then
-    exit 0
+  if [[ $new_brightness -ge $min_brightness ]]; then
+    brt_change "$new_brightness"
   else
-    exit 1
-  fi
-}
-
-exceeds_min() {
-  local new_brightness=$1
-  if [[ $new_brightness -lt $max_brightness ]]; then
-    exit 0
-  else
-    exit 1
+    brt_change "$min_brightness"
   fi
 }
 
 if [[ $# -eq 1 ]]; then
-  case $1 in
-    ^[0-9]*) brt_change "$1" && exit 0;;
-    up) brt_up;;
-    down) brt_down;;
-    *) echo "blah" && exit 1;;
+  case "$1" in
+    [0-9]*) brt_change "$1" && exit 0;;
+    up) brt_up && exit 0;;
+    down) brt_down && exit 0;;
+    *) echo "Error: invalid argument. Pick a brightness level ($min_brightness-$max_brightness), or say 'up' or 'down'." && exit 1;;
   esac
+elif [[ $# -eq 0 ]]; then
+  echo "Error: needs an argument."
+  exit 1
 else
-  echo "BLAH" && exit 1
+  echo "Error: too many arguments."
+  exit 1
 fi
