84f57ae4b6f618ba1ea4435c5a3f549adead9d4c
Brian Buccola <brian.buccola@gmail.com>
2017-02-18 18:12:58 -0500

    option "ctrl:nocaps" not necessary(?)

--- keyboard-adjust.sh
+++ keyboard-adjust.sh
@@ -10,4 +10,3 @@ setxkbmap -layout dvp,de,it,il          # programmers-dvorak kbd primary
 setxkbmap -option                       # clear options
 setxkbmap -option "grp:shifts_toggle"   # use both shifts to toggle layout
 setxkbmap -option "ctrl:swapcaps"       # swap ctrl_L and caps lock
-setxkbmap -option "ctrl:nocaps"
