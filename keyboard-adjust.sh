5b9fdb5f8c9479b4cbaf09067350236ceb8eb5b9
Brian Buccola <brian.buccola@gmail.com>
2017-02-19 08:29:58 -0500

    use '-layout us -variant dvp' instead of programmers-dvorak AUR package

--- keyboard-adjust.sh
+++ keyboard-adjust.sh
@@ -6,7 +6,7 @@
 #
 # description:      Set up kbd layouts, and switch caps lock with ctrl_L
 
-setxkbmap -layout dvp,de,it,il          # programmers-dvorak kbd primary
-setxkbmap -option                       # clear options
-setxkbmap -option "grp:shifts_toggle"   # use both shifts to toggle layout
-setxkbmap -option "ctrl:swapcaps"       # swap ctrl_L and caps lock
+setxkbmap -layout us,de,it,il -variant dvp,,, # programmers-dvorak kbd primary
+setxkbmap -option                             # clear options
+setxkbmap -option "grp:shifts_toggle"         # use both shifts to toggle layout
+setxkbmap -option "ctrl:swapcaps"             # swap ctrl_L and caps lock
