58ce56fb61f166b88b2486c38b489df0ff94d60e
Brian Buccola <brian.buccola@gmail.com>
2017-03-08 16:49:29 +0200

    update device name: wlp1s0 -> wlp3s0

--- wireless.sh
+++ wireless.sh
@@ -7,18 +7,18 @@
 #                 or to wifi, or to nothing). Useful for putting in a status
 #                 bar.
 
-iwconfig wlp1s0 2>&1 | grep -q no\ wireless\ extensions\. && {
+iwconfig wlp3s0 2>&1 | grep -q no\ wireless\ extensions\. && {
   echo wired
   exit 0
 }
 
-iwconfig wlp1s0 2>&1 | grep -q ESSID:off && {
+iwconfig wlp3s0 2>&1 | grep -q ESSID:off && {
   echo "(no wifi)"
   exit 0
 }
 
-essid=$(iwconfig wlp1s0 | awk -F '"' '/ESSID/ {print $2}')
-quality=$(iwconfig wlp1s0 | awk -F '=' '/Quality/ {print $2}' | cut -d ' ' -f 1)
+essid=$(iwconfig wlp3s0 | awk -F '"' '/ESSID/ {print $2}')
+quality=$(iwconfig wlp3s0 | awk -F '=' '/Quality/ {print $2}' | cut -d ' ' -f 1)
 quality_pct=$(echo "$quality * 100" | bc -l | sed 's/\.[0-9]*//')
 
 echo $essid $quality_pct\%
