055a542bca155faa86b769ad8aefc855304793b8
Brian Buccola <brian.buccola@gmail.com>
2017-03-08 16:48:43 +0200

    remove references to sudo

--- wireless.sh
+++ wireless.sh
@@ -5,21 +5,20 @@
 #
 # description:    Output connection status (whether connected to ethernet,
 #                 or to wifi, or to nothing). Useful for putting in a status
-#                 bar. Relies on `iwconfig`, which requires root privileges to
-#                 output full info.
+#                 bar.
 
-sudo iwconfig wlp1s0 2>&1 | grep -q no\ wireless\ extensions\. && {
+iwconfig wlp1s0 2>&1 | grep -q no\ wireless\ extensions\. && {
   echo wired
   exit 0
 }
 
-sudo iwconfig wlp1s0 2>&1 | grep -q ESSID:off && {
+iwconfig wlp1s0 2>&1 | grep -q ESSID:off && {
   echo "(no wifi)"
   exit 0
 }
 
-essid=$(sudo iwconfig wlp1s0 | awk -F '"' '/ESSID/ {print $2}')
-quality=$(sudo iwconfig wlp1s0 | awk -F '=' '/Quality/ {print $2}' | cut -d ' ' -f 1)
+essid=$(iwconfig wlp1s0 | awk -F '"' '/ESSID/ {print $2}')
+quality=$(iwconfig wlp1s0 | awk -F '=' '/Quality/ {print $2}' | cut -d ' ' -f 1)
 quality_pct=$(echo "$quality * 100" | bc -l | sed 's/\.[0-9]*//')
 
 echo $essid $quality_pct\%
