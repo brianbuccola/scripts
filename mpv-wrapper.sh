9108ffce80394b62b4ffa6cdbdb61b40be052520
Brian Buccola <brian.buccola@gmail.com>
2017-03-08 16:38:59 +0200

    remove old scripts no longer used

--- mpv-wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# file:          mpv-wrapper.sh
-# author:        Brian Buccola
-#
-# description:   Stream videos using mpv/youtube-dl. Useful for binding a key
-#                to in a browser capable of running external shell scripts.
-
-mpv --fs "$(xsel)"
